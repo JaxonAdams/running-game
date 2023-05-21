3876e8480e438d617b7b5ec653112bfcfd03ff52
Jaxon Adams <jadamsresume1@gmail.com>
2023-05-21 15:13:11 -0600

    basic window set up to display

--- game.py
+++ game.py
@@ -1,1 +1,15 @@
-# TODO: write some code
+import pygame
+
+# initialize pygame
+pygame.init()
+
+# set up game window
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Runner')
+
+# event loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
