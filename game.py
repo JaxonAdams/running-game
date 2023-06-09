982b30f9f99102b0b4cfa6805480579c6bdff412
Jaxon Adams <jadamsresume1@gmail.com>
2023-06-09 16:56:57 -0600

    began work on main menu screen

--- game.py
+++ game.py
@@ -1,13 +1,23 @@
 import pygame
 
 
-def display_score():
-    current_time = pygame.time.get_ticks() // 1000 - start_time
+def calc_score():
+    return pygame.time.get_ticks() // 1000 - start_time
 
-    score_surf = game_font.render(f'Score: {current_time}', False, (64, 64, 64))
+def display_score(score):
+    score_surf = game_font.render(f'Score: {score}', False, (64, 64, 64))
     score_rect = score_surf.get_rect(center=(400, 50))
 
     screen.blit(score_surf, score_rect)
+
+def display_menu(score):
+    screen.fill((94, 129, 162))
+    display_score(score)
+    screen.blit(player_stand, player_stand_rect)
+
+    start_txt = game_font.render('Press the spacebar to start!', False, (64, 64, 64))
+    start_txt_rect = start_txt.get_rect(center=(400, 350))
+    screen.blit(start_txt, start_txt_rect)
 
 
 # initialize pygame
@@ -15,9 +25,10 @@ pygame.init()
 
 # set up game window
 screen = pygame.display.set_mode((800,400))
-pygame.display.set_caption('Runner')
+pygame.display.set_caption('Pixel Runner')
 game_font = pygame.font.Font('./font/Pixeltype.ttf', 50)
 start_time = 0
+score = 0
 
 # set up game clock
 clock = pygame.time.Clock()
@@ -36,13 +47,18 @@ snail_surf = pygame.image.load('./graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomleft=(800, 300))
 
 # player
-player_surf = pygame.image.load('./graphics/player/player_stand.png').convert_alpha()
+# game
+player_surf = pygame.image.load('./graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom=(150, 300))
 player_gravity = 0
+# menu
+player_stand = pygame.image.load('./graphics/player/player_stand.png').convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand,0,2)
+player_stand_rect = player_stand.get_rect(center=(400,200))
 
 # event loop
 running = True
-game_active = True
+game_active = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -72,7 +88,8 @@ while running:
         screen.blit(ground_surface, (0, 300))
 
         # score/title
-        display_score()
+        score = calc_score()
+        display_score(score)
         # screen.blit(score_surf, score_rect)
 
         # render snail enemy
@@ -91,7 +108,9 @@ while running:
         if snail_rect.colliderect(player_rect):
             game_active = False
     else:
-        screen.fill('Cyan')
+        # menu screen
+        display_menu(score)
+
 
     pygame.display.update()
     clock.tick(60)
